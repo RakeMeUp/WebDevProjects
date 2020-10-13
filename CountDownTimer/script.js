b3fa95e05ec62d46019945a34d6327e9637bf6d3
Rake <bencehetes@gmail.com>
2020-10-13 22:21:53 +0200

    fixed hour timer, added the local timezone

--- CountDownTimer/script.js
+++ CountDownTimer/script.js
@@ -3,7 +3,7 @@ const hoursElement = document.getElementById('hours');
 const minsElement = document.getElementById('mins');
 const secondsElement = document.getElementById('seconds');
 
-const endtime = '1 Jan 2021';
+const endtime = 'January 1, 2021 GMT+02:00';
 
 const countDown = (endtime) => {
     //We get the total millisecs between current time and our set endtime
@@ -12,24 +12,24 @@ const countDown = (endtime) => {
         Using the modulo, the result only can be between 0-59
         Also we have to floor it because we dont need fractions of secs */
 
-    //im too lazy to understand the rest of the logic, i just copypasted it lol
     const seconds = Math.floor( (total/1000) % 60);
     const minutes = Math.floor( (total/1000/60) % 60);
-    const hours = Math.floor( (total/1000*60*60) % 24);
+    const hours = Math.floor( (total/1000/60/60) % 24);
     const days = Math.floor( total/(1000*60*60*24) );
 
     daysElement.innerHTML       = days;
     hoursElement.innerHTML      = formatTime(hours);
     minsElement.innerHTML       = formatTime(minutes);
     secondsElement.innerHTML    = formatTime(seconds);
+
+    console.log('total:' + total)
 }
 
 function formatTime(time){
     return time < 10 ? `0${time}` : time;
 }
 
-
 //initial call, so when we enter the site, we dont have to wait 1000ms
-countDown(endtime)
+ countDown(endtime)
 
 setInterval(function(){countDown(endtime);}, 1000);
